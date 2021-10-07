fcb631a959ac0c29b2130e3fa5b884f874d21152
Aleksi Tarasov <alexey2g@yandex.ru>
2021-10-08 00:26:05 +0300

    create cors

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const { errors } = require('celebrate');
@@ -25,6 +26,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
