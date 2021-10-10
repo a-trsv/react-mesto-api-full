da81751243da24af3fabeabdd846a757fd6921ac
Aleksi Tarasov <alexey2g@yandex.ru>
2021-10-10 16:10:31 +0300

    test crash test

--- backend/app.js
+++ backend/app.js
@@ -54,7 +54,8 @@ app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
-});
+})
+
 app.post('/signin', signInValidation, login);
 app.post('/signup', signUpValidation, createUser);
 app.use(auth);
