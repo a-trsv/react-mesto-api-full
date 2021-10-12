e90711a3727d0b826b26d2df8050ff4fcaeee8e5
Aleksi Tarasov <alexey2g@yandex.ru>
2021-10-12 09:55:22 +0300

    delete comm section in app.js

--- backend/app.js
+++ backend/app.js
@@ -41,10 +41,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUTP, PATCH, POST, DELETE, OPTIONS');
-  // if (req.method === 'OPTIONS') {
-  //   res.status(200).send;
-  //   return;
-  // }
   next();
 });
 app.use(express.json());
